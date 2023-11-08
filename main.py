536af07e1a9d9c13ec679c357055471a27db46fe
Greeg <minikipro@gmail.com>
2023-11-08 05:24:24 +0400

    Add visualize towers, path, city on map

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class CityGrid:
         self.M = M
         self.grid = np.zeros((N, M), dtype=int)
         self.towers = []  # Список для хранения размещенных башен
+        self.budget = 0
 
         # Инициализируем блоки с преградами
         for i in range(N):
@@ -18,17 +19,27 @@ class CityGrid:
 
     def place_tower(self, x, y, range_R, cost):
         # Размещаем башню и визуализируем ее зону покрытия
-        self.towers.append((x, y, range_R, cost))  # Добавляем башню в список
-        for i in range(max(0, x - range_R), min(self.N, x + range_R + 1)):
-            for j in range(max(0, y - range_R), min(self.M, y + range_R + 1)):
-                if (x - i) ** 2 + (y - j) ** 2 <= range_R ** 2 and self.grid[i][j] != 1:
-                    self.grid[i][j] = 2  # Помечаем как покрытие башни
+        if cost <= self.budget:
+            self.towers.append((x, y, range_R, cost))  # Добавляем башню в список
+            self.budget -= cost
+            for i in range(max(0, x - range_R), min(self.N, x + range_R + 1)):
+                for j in range(max(0, y - range_R), min(self.M, y + range_R + 1)):
+                    if (x - i) ** 2 + (y - j) ** 2 <= range_R ** 2 and self.grid[i][j] != 1:
+                        self.grid[i][j] = 2  # Помечаем как покрытие башни
+            print(f"Placed tower at ({x}, {y}) with range {range_R} and cost {cost}. Budget remaining: {self.budget}")
 
-    def optimize_tower_placement(self, range_R, cost):
-        # Реализуем алгоритм размещения башен, чтобы покрыть непрегражденные блоки
-        for i in range(self.N):
-            for j in range(self.M):
+    def optimize_tower_placement(self, budget):
+        self.budget = budget
+        self.towers = []  # Сбросим уже размещенные башни
+        radius_step = 2  # Радиус башни используется как шаг для равномерного размещения
+
+        for i in range(0, self.N, radius_step):
+            for j in range(0, self.M, radius_step):
                 if self.grid[i][j] != 1:
+                    # Рассчитываем стоимость башни в данной ячейке (просто случайное значение в данном примере)
+                    cost = random.randint(1, 10)
+                    # Рассчитываем радиус башни в данной ячейке (просто случайное значение в данном примере)
+                    range_R = random.randint(1, 5)
                     self.place_tower(i, j, range_R, cost)
 
     def find_reliable_path(self, tower1, tower2):
@@ -78,8 +89,10 @@ class CityGrid:
             current = prev[current]
 
         if path:
+            print(f"Надежный путь от {tower1} к {tower2}: {path}")
             return path
         else:
+            print(f"Надежный путь от {tower1} к {tower2} не найден.")
             return None  # Нет пути между башнями
 
     def calculate_reliability(self, tower1, tower2):
@@ -99,38 +112,36 @@ class CityGrid:
         return distance
 
     def visualize(self, aspect='city', path=None):
-        # Визуализация сетки города
+        plt.figure()
         if aspect == 'city':
-            plt.imshow(self.grid, cmap='viridis', interpolation='nearest')
-            plt.show()
-        # Визуализация башен
+            plt.imshow(self.grid, cmap='viridis', interpolation='nearest', origin='lower')
         elif aspect == 'towers':
+            plt.imshow(self.grid, cmap='viridis', interpolation='nearest', origin='lower')
             tower_locations = np.array([tower[:2] for tower in self.towers])
             plt.scatter(tower_locations[:, 1], tower_locations[:, 0], marker='s', c='r', label='Towers')
+            for x, y, _, _ in self.towers:
+                plt.text(y, x, f'Tower({y}, {x})', fontsize=8, color='r')
             plt.legend()
-            plt.show()
-        # Визуализация надежного пути между башнями
-        elif aspect == 'reliable_path' and path:
+        elif aspect == 'reliable_path':
             reliable_path = np.copy(self.grid)
             for x, y in path:
                 reliable_path[x, y] = 3
-            plt.imshow(reliable_path, cmap='viridis', interpolation='nearest')
-            plt.show()
+            plt.imshow(reliable_path, cmap='viridis', interpolation='nearest', origin='lower')
+            for x, y in path:
+                plt.text(y, x, f'Path({y}, {x})', fontsize=8, color='b')
+        plt.show()
 
 # Пример использования
 city = CityGrid(10, 10)
-city.optimize_tower_placement(3, 100)  # Настроим радиус и стоимость здесь
+budget = 50
+city.optimize_tower_placement(budget)
 city.visualize(aspect='city')
 
-# Размещение башен в координатах (3, 4) и (6, 7) с радиусами и стоимостью
-city.place_tower(4, 4, 3, 100)
-city.place_tower(6, 7, 2, 80)
-
-# Визуализация размещенных башен и надежного пути между ними
+# Выберем две случайные башни для поиска надежного пути
+tower1 = random.choice(city.towers)[:2]
+tower2 = random.choice(city.towers)[:2]
 city.visualize(aspect='towers')
+path = city.find_reliable_path(tower1, tower2)
 
-tower1 = city.towers[0]
-tower2 = city.towers[1]
-reliable_path = city.find_reliable_path(tower1[:2], tower2[:2])
-if reliable_path:
-    city.visualize(aspect='reliable_path', path=reliable_path)
+if path:
+    city.visualize(aspect='reliable_path', path=path)
